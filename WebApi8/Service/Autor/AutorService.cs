2a35944157a95ecf3887f1f664e85e034806b410
Kevin Lopes Costa <kevinlopes3012@gmail.com>
2024-08-15 13:13:27 -0400

    Adicionar arquivos de projeto.

---
+++ WebApi8/Service/Autor/AutorService.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WebApi8.Data;
+using WebApi8.Dto.Autor;
+using WebApi8.Models;
+
+namespace WebApi8.Service.Autor
+{
+    public class AutorService : IAutorInterface
+    {
+        private readonly AppDbContext _context;
+        public AutorService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idAutor)
+        {
+            ResponseModel<AutorModel> resposta = new ResponseModel<AutorModel>();
+            try
+            {
+
+                var autor = await _context.Autores.FirstOrDefaultAsync(autorBanco => autorBanco.Id == idAutor);
+                if (autor == null) 
+                {
+                    resposta.Mensagem = "Nenhum Autor localizado!";
+                    return resposta;
+                }
+
+                resposta.Dados = autor;
+                resposta.Mensagem = "autor foi localizado!";
+
+                return resposta;
+
+            }
+            catch (Exception ex)
+            {
+                resposta.Mensagem = ex.Message;
+                resposta.Status = false;
+                return resposta;
+            }
+        }
+
+        public async Task<ResponseModel<AutorModel>> BuscarAutorPorIdLivro(int idLivro)
+        {
+            ResponseModel<AutorModel> resposta = new ResponseModel<AutorModel>();
+            try
+            {
+
+                var livro = await _context.Livros.Include(a => a.Autor).FirstOrDefaultAsync(livroBanco => livroBanco.Id == idLivro) ;
+                if (livro == null)
+                {
+                    resposta.Mensagem = "Nenhum Autor localizado!";
+                    return resposta;
+                }
+                resposta.Dados = livro.Autor;
+                resposta.Mensagem = "Autor localizado!";
+
+                return resposta;
+
+            }
+            catch (Exception ex)
+            {
+                resposta.Mensagem = ex.Message;
+                resposta.Status = false;
+                return resposta;
+            }
+        }
+
+        public async Task<ResponseModel<List<AutorModel>>> CriarAutor(AutorCriacaoDto autorCriacaoDto)
+        {
+            ResponseModel<List<AutorModel>> resposta = new ResponseModel<List<AutorModel>>();
+            try
+            {
+
+                var autor = new AutorModel()
+                {
+                    Nome = autorCriacaoDto.Nome,
+                    SobreNome = autorCriacaoDto.SobreNome
+                };
+                _context.Add(autor);
+                await _context .SaveChangesAsync();
+
+                resposta.Dados = await _context.Autores.ToListAsync();
+                resposta.Mensagem = "Autor criado com sucesso!";
+
+                return resposta;
+
+            }
+            catch (Exception ex)
+            {
+                resposta.Mensagem = ex.Message;
+                resposta.Status = false;
+                return resposta;
+            }
+        }
+
+        public async Task<ResponseModel<List<AutorModel>>> EditarAutor(AutorEdicaoDto autorEdicaoDto)
+        {
+            ResponseModel<List<AutorModel>> resposta = new ResponseModel<List<AutorModel>>();
+            try
+            {
+                var autor = await _context.Autores
+                    .FirstOrDefaultAsync(autorbanco => autorbanco.Id == autorEdicaoDto.Id);
+                if (autor == null)
+                {
+                    resposta.Mensagem = "Nenhum Autor localizado!";
+                    return resposta;
+                }
+                autor.Nome = autorEdicaoDto.Nome;
+                autor.SobreNome = autorEdicaoDto.SobreNome;
+
+                _context.Update(autor);
+                await _context.SaveChangesAsync();
+
+                resposta.Dados = await _context.Autores.ToListAsync();
+                resposta.Mensagem = "autor foi alterado!";
+
+                return resposta;
+
+            }
+            catch (Exception ex)
+            {
+                resposta.Mensagem = ex.Message;
+                resposta.Status = false;
+                return resposta;
+            }
+        }
+
+        public async Task<ResponseModel<List<AutorModel>>> ExcluirAutor(int idAutor)
+        {
+            ResponseModel<List<AutorModel>> resposta = new ResponseModel<List<AutorModel>>();
+            try
+            {
+
+                var autor = await _context.Autores.FirstOrDefaultAsync(autorBanco => autorBanco.Id == idAutor);
+                if (autor == null)
+                {
+                    resposta.Mensagem = "Nenhum Autor localizado!";
+                    resposta.Status =true;
+                    return resposta;
+                }
+                _context.Remove(autor);
+                await _context.SaveChangesAsync();
+
+                resposta.Dados = await _context.Autores.ToListAsync();
+                resposta.Mensagem = "autor foi apagado!";
+
+                return resposta;
+
+            }
+            catch (Exception ex)
+            {
+                resposta.Mensagem = ex.Message;
+                resposta.Status = false;
+                return resposta;
+            }
+        }
+
+        public async Task<ResponseModel<List<AutorModel>>> ListarAutores()
+        {
+            ResponseModel < List < AutorModel >> resposta = new ResponseModel<List<AutorModel>>();
+            try
+            {
+
+                var autores = await _context.Autores.ToListAsync();
+                
+                resposta.Dados = autores;
+                resposta.Mensagem = "Todos os autores foram coletados!";
+
+                return resposta;
+
+            }
+            catch (Exception ex)
+            {
+                resposta.Mensagem = ex.Message;
+                resposta.Status = false;
+                return resposta;
+            }
+        }
+    }
+}
